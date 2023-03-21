4e3def9c77fe13bec993d6676018f8a27861f729
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-03-21 12:38:10 -0300

    first

---
+++ decimal_to_roma.py
@@ -0,0 +1,13 @@
+import unittest
+
+def roman_to_decimal(roman)
+if roman == I:
+    return "1"
+
+class Testromantodecimal (unittest.TestCase):
+    def test_uno(self):
+        resultado = roman_to_decimal(1)
+        self.assertEqual(resultado, "I")
+
+if __name__==  "__main__":
+    unittest.main()
