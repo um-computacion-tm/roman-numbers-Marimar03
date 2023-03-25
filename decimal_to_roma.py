43706fe91b0e80b523b16879188c8eb50d325444
Marimar03 <md.acuna@alumno.um.edu.ar>
2023-03-24 23:18:32 -0300

    ultima version

--- decimal_to_roma.py
+++ decimal_to_roma.py
@@ -1,13 +1,31 @@
 import unittest
 
-def roman_to_decimal(roman)
-if roman == I:
-    return "1"
+def decimal_to_roman(decimal):
+    total = ""
+    if decimal >= 100:
+        centena = decimal // 100
+        total = "C" * centena 
+        decimal = decimal % 100
+    if decimal <= 3:
+        total += "I" * decimal 
+    elif decimal == 5:
+        total += "V" * decimal 
+    elif decimal == 10:
+        total += "X"
+    return total 
 
-class Testromantodecimal (unittest.TestCase):
-    def test_uno(self):
-        resultado = roman_to_decimal(1)
-        self.assertEqual(resultado, "I")
+class TestDecimalToRoman (unittest.TestCase):
+    def test_uno (self):
+        resultado = decimal_to_roman (1)
+        self.assertEqual (resultado, "I")
+    
+    def test_diez (self):
+        resultado = decimal_to_roman (10)
+        self.assertEqual (resultado, "X")
 
-if __name__==  "__main__":
+    def test_cinco (self):
+        resultado = decimal_to_roman (5)
+        self.assertEqual (resultado, "V")
+
+if __name__ == "__main__":
     unittest.main()
